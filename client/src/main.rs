3d0beee517229aaa37b386b22584ac5f6085f02f
Alistair Campbell <ajcampbell@conestogac.on.ca>
2025-03-24 15:54:21 -0400

    feat: Set up command line parsing

--- client/src/main.rs
+++ client/src/main.rs
@@ -1,3 +1,25 @@
+use std::env;
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+    let client_id: String = args[1].clone();
+    let start_pos = args[2].clone();
+    let end_pos = args[3].clone();
+    let plane_speed = args[4].clone().parse::<i32>().unwrap();
+
+
+    //enter flight loop
+    loop {
+        //move aircraft
+
+        //if distance to destination is less than A VALUE (idk what)
+
+        break;
+        
+        //send data
+
+        //wait for 5 seconds
+    }
+
+    //send big data
 }
