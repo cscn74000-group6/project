bea2873c312c89b7a69cdccfefcf8a77b39e0bf8
Alistair Campbell <ajcampbell@conestogac.on.ca>
2025-03-24 17:01:18 -0400

    feat: added vector math lib and converted to using vectors

--- client/src/main.rs
+++ client/src/main.rs
@@ -1,12 +1,20 @@
 use std::env;
+use vecmath::{self, Vector3};
 
 fn main() {
     let args: Vec<String> = env::args().collect();
     let client_id: String = args[1].clone();
-    let start_pos = args[2].clone();
-    let end_pos = args[3].clone();
-    let plane_speed = args[4].clone().parse::<i32>().unwrap();
-
+    let start_pos: Vector3<i32> = [
+        args[2].clone().parse::<i32>().unwrap(),
+        args[3].clone().parse::<i32>().unwrap(),
+        args[4].clone().parse::<i32>().unwrap(),
+    ];
+    let end_pos: Vector3<i32> = [
+        args[5].clone().parse::<i32>().unwrap(),
+        args[6].clone().parse::<i32>().unwrap(),
+        args[7].clone().parse::<i32>().unwrap(),
+    ];
+    let plane_speed = args[8].clone().parse::<i32>().unwrap();
 
     //enter flight loop
     loop {
@@ -15,7 +23,7 @@ fn main() {
         //if distance to destination is less than A VALUE (idk what)
 
         break;
-        
+
         //send data
 
         //wait for 5 seconds
