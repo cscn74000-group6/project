a096b80d97ccd279068488d5fe6d1d0b93c0ff15
Sebastian Ibanez <sebas.ibanez219@gmail.com>
2025-03-25 03:09:07 -0400

    feat: create Manager and ClientHandler

--- client/src/main.rs
+++ client/src/main.rs
@@ -1,3 +1,25 @@
-fn main() {
-    println!("Hello, world!");
+use tokio::io::AsyncWriteExt;
+use tokio::net::TcpStream;
+
+
+#[tokio::main]
+async fn main() {
+    loop {
+        match TcpStream::connect("127.0.0.1:8001").await {
+            Ok(mut stream) => {
+                println!("Successfully connected to server");
+                match stream.write_u8(42).await {
+                    Ok(_) => {
+                        println!("Successfully sent data");
+                    }
+                    Err(e) => {
+                        println!("Failed to write to server: {e}");
+                    }
+                }
+            }
+            Err(e) => {
+                println!("Failed to connect to server: {e}");
+            }
+        }
+    }
 }
