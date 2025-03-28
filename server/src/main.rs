99ec0d6c9f6e12ffb055949fe51dfda1027406d0
Inventhrice <45127310+Inventhrice@users.noreply.github.com>
2025-03-28 17:47:19 -0400

    Putting the logging file in

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,10 +1,17 @@
 use crate::manager::Manager;
-
+use tracing;
 pub mod client_handler;
 pub mod manager;
 
 #[tokio::main]
 async fn main() {
+    let appender = tracing_appender::rolling::never("./log", "server.log");
+    let (non_blocking_appender, _guard) = tracing_appender::non_blocking(appender);
+
+    tracing_subscriber::fmt()
+        .with_writer(non_blocking_appender)
+        .init();
+    tracing::error!("Catch me!");
     let manager = Manager::new();
     manager.run().await;
 }
