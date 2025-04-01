2c48424fede8123e7e46966d72e502072978420f
Ajdj100 <ajcampbell@conestogac.on.ca>
2025-04-01 07:25:10 -0400

    fix: tweaked usage of computed values causing packet corruption

--- utils/src/packet.rs
+++ utils/src/packet.rs
@@ -34,7 +34,7 @@ pub struct PacketHeader {
     pub flag: FlagState,
     pub plane_id: u8,
     pub body_size: u16,
-    pub seq_len: u8
+    pub seq_len: u8,
 }
 
 impl PacketHeader {
@@ -45,13 +45,14 @@ impl PacketHeader {
             flag: FlagState::WARNING,
             plane_id: 0,
             body_size: 0,
-            seq_len: 0
+            seq_len: 0,
         };
     }
     /// Deseralize_packet_header() takes in a u8 slice and returns an unpacked PacketHeader. The
     /// function deseralizes in the same way the serialize_packet_header works.
     pub fn deseralize_packet_header(stream: &[u8]) -> Result<PacketHeader, std::io::Error> {
-        if stream.len() < std::mem::size_of::<PacketHeader>() {
+        // if stream.len() < std::mem::size_of::<PacketHeader>() {
+        if stream.len() < 5 {
             return Err(std::io::Error::new(
                 std::io::ErrorKind::InvalidInput,
                 "Vector does not have enought elements for a PacketHeader",
@@ -64,12 +65,14 @@ impl PacketHeader {
         let seq_len = stream.get(4).copied();
 
         match (new_flag, plane_id, body_1, body_2, seq_len) {
-            (Some(new_flag), Some(plane_id), Some(body_1), Some(body_2), Some(seq_len)) => Ok(PacketHeader {
-                flag: FlagState::init(new_flag),
-                plane_id,
-                body_size: u16::from_ne_bytes([body_1, body_2]),
-                seq_len
-            }),
+            (Some(new_flag), Some(plane_id), Some(body_1), Some(body_2), Some(seq_len)) => {
+                Ok(PacketHeader {
+                    flag: FlagState::init(new_flag),
+                    plane_id,
+                    body_size: u16::from_ne_bytes([body_1, body_2]),
+                    seq_len,
+                })
+            }
             _ => Err(std::io::Error::new(
                 std::io::ErrorKind::InvalidInput,
                 "Unable to parse PacketHeader fields from Vec<u8>",
@@ -84,7 +87,6 @@ impl PacketHeader {
         seralized_bytes.push(self.plane_id);
         seralized_bytes.extend_from_slice(&self.body_size.to_ne_bytes());
         seralized_bytes.push(self.seq_len);
-        dbg!(&seralized_bytes);
         seralized_bytes
     }
 }
@@ -150,9 +152,11 @@ pub fn serialize_packet(pkt: Packet, stream: &mut TcpStream) -> Result<(), std::
     let mut seralized_bytes: Vec<u8> = Vec::new();
     seralized_bytes.extend(pkt.header.seralize_packet_header());
     seralized_bytes.extend_from_slice(&pkt.body);
-    dbg!(&seralized_bytes[0..20]);                  //data is intact here
     match stream.try_write(&seralized_bytes) {
-        Ok(_) => Ok(()),
+        Ok(v) => {
+            println!("{v}");
+            Ok(())
+        }
         Err(e) => Err(e.into()),
     }
 }
@@ -160,15 +164,11 @@ pub fn serialize_packet(pkt: Packet, stream: &mut TcpStream) -> Result<(), std::
 /// Takes in the TcpStream, reads values from it, and returns a Packet deseralized.
 /// The logic works, except for the TcpStream which is still untested.
 pub async fn deserialize_packet(stream: &mut TcpStream) -> Result<Packet, std::io::Error> {
-    let mut rcv_buf_header: Vec<u8> = vec![0; std::mem::size_of::<PacketHeader>()];
+    // let mut rcv_buf_header: Vec<u8> = vec![0; std::mem::size_of::<PacketHeader>()];
+    let mut rcv_buf_header: Vec<u8> = vec![0; 5];
     let mut pkt: Packet = Packet::init();
-
     // Read the data from the buffer
     stream.read_exact(&mut rcv_buf_header).await?;
-
-
-    dbg!(&rcv_buf_header);                   //data is corrupt here
-
 
     pkt.header = match PacketHeader::deseralize_packet_header(&rcv_buf_header) {
         Ok(header) => header,
@@ -177,7 +177,7 @@ pub async fn deserialize_packet(stream: &mut TcpStream) -> Result<Packet, std::i
         }
     };
 
-    let mut rcv_buf: Vec<u8> = vec![0; pkt.header.bDody_size.into()];
+    let mut rcv_buf: Vec<u8> = vec![0; pkt.header.body_size.into()];
     stream.read_exact(&mut rcv_buf).await?;
     pkt.body = rcv_buf;
 
