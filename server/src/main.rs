26914cb10b56fd0602789bf40903bed8a02ee2a0
Sebastian Ibanez <sebas.ibanez219@gmail.com>
2025-03-31 23:00:32 -0400

    feat: implement the server state machine

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,9 +1,11 @@
 use crate::manager::Manager;
 
 pub mod manager;
+pub mod state_machine;
 
 #[tokio::main]
 async fn main() {
+    // Initialize and run server manager.
     match Manager::new().run().await {
         Ok(_) => {
             println!("Manager exited gracefully...");
